af03c398697845d596f50e0d6992b1ceae1646a2
Michael <mvanderlyn27@gmail.com>
2017-04-17 20:54:36 -0400

    Worked on improving motor control server, got motor powered up

--- robot-control-server/bluetooth-motor-server-test.py
+++ robot-control-server/bluetooth-motor-server-test.py
@@ -9,35 +9,56 @@ import RPi.GPIO as GPIO
 ##############<METHODS>#####################
 def main():
     print("Initiating Motor Control Test")
-    hostMACAddress = 'B8:27:EB:4A:5A:58'
+#   hostMACAddress = 'B8:27:EB:4A:5A:58'
     port = 3
     backlog = 1
     size = 1024
     s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    s.bind((hostMACAddress, port))
+    s.bind(("",port)) # ((hostMACAddress, port))
     s.listen(backlog)
+    motors.enable()
+    motors.setSpeeds(0,0)
     try:
         client, clientInfo = s.accept()
-        motors.enable()
-        motors.setSpeeds(0,0)
-        if data:
-            if data == "w":
-                driveForward(MAX_SPEED/2)
-            elif data == "s":
-                driveBackward(MAX_SPEED/2)
-            elif data == "q":
-                client.close()
-                s.close()
-                quit
+        while(1):
+            print("test1")
+            data = client.recv(1024)
+            if data:
+                if data == "w":
+                    print("forward")
+                    driveForward(MAX_SPEED/2)
+                elif data == "s":
+                    print("reverse")
+                    driveBackward(MAX_SPEED/2)
+                elif data == "q":
+                    print("quit")
+                    client.close()
+                    s.close()
+                    break
+                else:
+                    print('stopped')
+                    stopMotors()
             else:
+                print('no data')
                 stopMotors()
-    except:
+                time.sleep(0.005)
+            time.sleep(1/60)
+            print("cyle")
+    except Exception as e:
+        print(str(e))
         print("Closing socket")
-        client.close()
-        s.close()
-    finally:
         motors.setSpeeds(0,0)
         motors.disable()
+        client.close()
+        s.close()
+        quit
+    finally:
+#        print("closing test")
+        motors.setSpeeds(0,0)
+        motors.disable()
+#        client.close()
+#        s.close()
+#        quit
 def driveForward(speed):
     motors.motor1.setSpeed(speed)
     time.sleep(0.005)
